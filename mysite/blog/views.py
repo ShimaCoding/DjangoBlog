4ffe14c797a53e802a699a496bc32a92dc60ac9c
ShimaCoding <shimacoding@gmail.com>
2021-07-20 15:12:38 -0400

    unit 3 complete

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,37 +1,36 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post,Comment
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm,SearchForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 # Create your views here.
 
 
-def post_list(request,tag_slug=None):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
     tag = None
-    
+
     if tag_slug:
-        tag = get_object_or_404(Tag,slug=tag_slug)
+        tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
-    paginator = Paginator(object_list, 3) # 3 post in each page
+    paginator = Paginator(object_list, 3)  # 3 post in each page
     page = request.GET.get('page')
     try:
-        posts= paginator.page(page)
+        posts = paginator.page(page)
     except PageNotAnInteger:
-        #if page is not an integer deliver the first page
+        # if page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
 
-    
-    return render(request, 'blog/post/list.html', {'page':page,'posts': posts,'tag':tag})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
     """
 class PostListView(ListView):
@@ -48,12 +47,12 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    #List of active comments for this post
-    comments = post.comments.filter(active = True)
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
     new_comment = None
 
     if request.method == 'POST':
-        #A comment was posted
+        # A comment was posted
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             # Create Comment object but don't save to database yet
@@ -65,10 +64,12 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    #List of similar posts
-    post_tags_ids = Post.tags.values_list('id',flat=True)
-    similar_posts = Post.published.filter(tags__in = post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags = Count('tags')).order_by('-same_tags','-publish')[:4]
+    # List of similar posts
+    post_tags_ids = Post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'new_comment': new_comment,
@@ -76,17 +77,17 @@ def post_detail(request, year, month, day, post):
                                                      'similar_posts': similar_posts})
 
 
-def post_share(request,post_id):
-    #retrieve post by id
-    post = get_object_or_404(Post,id=post_id,status='published')
+def post_share(request, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
     if request.method == 'POST':
-        #Form was submited
+        # Form was submited
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            #Form fields passed validation
+            # Form fields passed validation
             cd = form.cleaned_data
-            #... send email
+            # ... send email
             post_url = request.build_absolute_uri(
                 post.get_absolute_url())
             subject = f"{cd['name']} recommends you read " \
@@ -98,7 +99,7 @@ def post_share(request,post_id):
             sent = True
     else:
         form = EmailPostForm()
-    return render(request,'blog/post/share.html',{'post':post,'form':form,'sent':sent})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 
 def post_search(request):
@@ -109,8 +110,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search = SearchVector('title','body'),).filter(search=query)
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body',weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
 
-    return render(request,'blog/post/search.html',{'form':form,
-                                                    'query':query,
-                                                    'results':results})
+    return render(request, 'blog/post/search.html', {'form': form,
+                                                     'query': query,
+                                                     'results': results})
